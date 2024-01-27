f4eb80e17d547cd85de76d87c8c130f32b995720
BuildTools <unconfigured@null.spigotmc.org>
2024-01-27 10:55:38 -0500

    ninja roen

--- src/JumpingBall.java
+++ src/JumpingBall.java
@@ -4,22 +4,19 @@ public class JumpingBall extends Ball {
     public JumpingBall(int x, int y, double diameter, Color color) {
         super(x, y, diameter, color);
     }
-    public double DistanceFromMarble(double x1,double x2, double y1,double y2) {
-
-        return Math.sqrt(Math.pow((x1 - x2),2) + Math.pow((y1 + y2),2));
+    public double DistanceFromMarble(double x2,double y2) {
+        double x1 = getX();
+        double y1 = getY();
+        return Math.sqrt(Math.pow((x1 - x2),2) + Math.pow((y1 - y2),2));
     }
-    public boolean isMarbleTouchingMango(Ball ball) {
+    public boolean isMarbleTouchingMango(Ball balls) {
         //getRadius() = jumping ball radius
         //ball.getRadius() = ball radius
-        if(DistanceFromMarble(getX(),ball.getX(),getY(),ball.getY()) <= getRadius() + ball.getRadius()) {
-            return true;
-        } else {
-            return false;
-        }
+        return DistanceFromMarble(balls.getX(), balls.getY()) <= balls.getRadius() + getRadius();
     }
 
     public void MoveTheCatFromTheBasket(int width, int height) {
-        setX((int)Math.random() * width);
-        setX((int)Math.random() * height);
+        setX((int)(Math.random() * width));
+        setX((int)(Math.random() * height));
     }
 }
